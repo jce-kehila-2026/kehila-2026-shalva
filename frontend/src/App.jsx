40d6f5a945aef833de84d0d7b1901cb4e08f6d7f
Shoval Vazana <shovalba@post.jce.ac.il>
2026-05-04 13:15:40 +0300

    login component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Login from './components/Login'
 
 function App() {
   
   return (
     <div>
-
+      <Login />
     </div>
       
   )
