d9b3e07fbb7fc1d610effaffe1aed96c2c70cd9f
lior97531 <lifshits.lior@gmail.com>
2026-05-15 14:01:48 +0300

    fixed login and made a demo admin dash
    the login is now working according to the user role by taking the id from the authentication and looking for the right id

---
+++ frontend/src/components/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+const AdminDashboard = () => {
+  return (
+    <div>
+      <button>button1</button>
+      <button>button2</button>
+      <button>button3</button>
+      <button>button4</button>
+    </div>
+  );
+};
+
+export default AdminDashboard;
